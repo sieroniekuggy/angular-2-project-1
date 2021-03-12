8ebac285b03c64824bafbd887e1a2dfd7a8dd961
sieroniekuggy <sieroniekuggy@gmail.com>
2021-03-12 13:21:27 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,3 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
-echo "# angular-2-project-1" >> README.md
-git init
-
-git commit -m "first commit"
-git branch -M master
-git remote add origin https://github.com/sieroniekuggy/angular-2-project-1.git
-git push -u origin master
